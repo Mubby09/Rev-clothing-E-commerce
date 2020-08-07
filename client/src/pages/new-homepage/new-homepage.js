d0348a6f39bf5ce8979e0b60d562c36a580ce74c
Mubarak <akinsolamubarak1996@gmail.com>
2020-08-07 13:42:25 +0100

    hompage adjusted

--- client/src/pages/new-homepage/new-homepage.js
+++ client/src/pages/new-homepage/new-homepage.js
@@ -6,6 +6,7 @@ const NewHomepage = ({ history }) => {
   return (
     <div className="background">
       <div className="heading">
+        <p className="top">Sign Up to view our collection</p>
         <h1>SHOP WITH US TODAY AT REV-CLOTHINGS</h1>
         <Button onClick={() => history.push("/signIn")}>
           SIGN IN OR SIGN UP HERE
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,12 +2,15 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const middlewares = [];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
-
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 export const persistor = persistStore(store);
